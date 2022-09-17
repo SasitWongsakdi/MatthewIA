3f00c69587d303c60412415a81dae98194fcb8bd
unknown <mwongsakdi@gmail.com>
2022-09-18 01:07:45 +0700

    step 1

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-  
-  const express = require('express')
-  const app = express()
-  const bodyParser = require('body-parser')
-  
-//   const indexRouter = require('./routes/index')
-//   const authorRouter = require('./routes/authors')
-//   const bookRouter = require('./routes/books')
-  
-  app.set('view engine', 'ejs')
-  
-app.get('/', (req, res) => {
-    //   res.send('Successful response.');
-      res.sendFile(__dirname + '/front.html');
-    });
-  app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
-  
-  const mongoose = require('mongoose')
-  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-  const db = mongoose.connection
-  db.on('error', error => console.error(error))
-  db.once('open', () => console.log('Connected to Mongoose'))
-  
-//   app.use('/', indexRouter)
-//   app.use('/authors', authorRouter)
-//   app.use('/books', bookRouter)
-  
-  app.listen(process.env.PORT || 3000)
+const express = require('express')
+
+const mongoose = require('mongoose')
+
+const app = express()
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'ejs')
+app.use('*/bookcover', express.static('./public/bookcover')); //Basically gives this thing html
+app.use('*/css', express.static('./public/css'));
+
+app.get("/", (req,res) =>{
+    res.render("front")
+})
+
+app.get("/addBook", (req,res) =>{
+    res.render("addBook")
+})
+app.get("/searchBook", (req,res) =>{
+    res.render("searchBook")
+})
+
+app.listen(3000, () => console.log('Server running on port 3000!'))
